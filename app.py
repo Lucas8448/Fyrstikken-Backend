19fe61c96eec89272c6c67787df9b5cd86cdc52f
Lucas William Bateson <lucas.bateson@gmail.com>
2024-06-17 10:33:03 +0200

    allow all origins

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from functools import wraps
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=["https://fyrstikken.vercel.app", "https://fyrstikken.f21elev.no"])
+CORS(app, supports_credentials=True, origins="*")
 dotenv.load_dotenv()
 
 DATABASE = 'voting.db'
