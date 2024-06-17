8a72ceeb3a2dbb679e46b4a043b26b2d3080459a
Lucas William Bateson <lucas.bateson@gmail.com>
2024-06-17 10:40:58 +0200

    Fix

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ def vote_results():
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'https://your-vercel-domain.vercel.app')
+    response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     response.headers.add('Access-Control-Allow-Credentials', 'true')
