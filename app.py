ccceb604a174e6b48799e0ddec62999ad2d5c7e3
Lucas William Bateson <lucas.bateson@gmail.com>
2024-06-17 10:20:08 +0200

    Add cert

--- app.py
+++ app.py
@@ -186,4 +186,5 @@ def vote_results():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, host='0.0.0.0')
+    context = ('cert.pem', 'key.pem')
+    app.run(host='0.0.0.0', port=5000, ssl_context=context)
