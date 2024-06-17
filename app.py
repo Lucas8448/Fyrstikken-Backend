cb54666c6463cab2a8a96ee2ef694d1eb4469817
Lucas William Bateson <lucas.bateson@gmail.com>
2024-06-17 10:39:34 +0200

    Fix

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ from functools import wraps
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+cors_config = {
+    "origins": ["https://fyrstikken.vercel.app", "https://fyrstikken.f21elev.no"]
+}
+CORS(app, resources={"/*": cors_config}, supports_credentials=True)
 dotenv.load_dotenv()
 
 DATABASE = 'voting.db'
